ad3aebb9ac56f6b5c14465d520cfc1dc6d8537f9
James Britton <brittonjp@gmail.com>
2019-06-09 20:22:02 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,6 +10,6 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self) unless @backers.include?(self)
+    project.add_backer(self) unless @backed.include?(self)
   end
 end
